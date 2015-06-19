da904e32d576d4ceeeb34e001f46faaaadfc6fb3
chaseadamsio <chase@realchaseadams.com>
2015-06-19 13:45:42 -0700

    comment out router for entry

--- src/js/entry.js
+++ src/js/entry.js
@@ -1,13 +1,13 @@
 require('../sass/app.scss');
-var React = require('react');
-var Router = require('react-router');
+// var React = require('react');
+// var Router = require('react-router');
 
 
-var Routes = require('./routes.jsx');
+// var Routes = require('./routes.jsx');
 
-Router.run(Routes, Router.HistoryLocation, (Root) => {
-  var initialProps = [];
-  React.render(React.createElement(Root, initialProps), document);
-});
+// Router.run(Routes, Router.HistoryLocation, (Root) => {
+//   var initialProps = [];
+//   React.render(React.createElement(Root, initialProps), document);
+// });
 
 require('./3party/googleAnalytics.js')();
