02246bc1851ee609615cfc517894e36be860beba
chaseadamsio <chase@realchaseadams.com>
2015-06-18 20:39:24 -0700

    add lib index

--- src/js/entry.js
+++ src/js/entry.js
@@ -1,1 +1,10 @@
 require('../sass/app.scss');
+var React = require('react');
+var Router = require('react-router');
+
+
+var Routes = require('./routes.jsx');
+
+Router.run(Routes, Router.HashLocation, (Root) => {
+  React.render(<Root/>, document.body);
+});React.render(<Routes />, document.getElementById('main'));
---
+++ src/js/lib/index.js
@@ -0,0 +1,5 @@
+var React = require('react');
+var Router = require('react-router');
+var Routes = require('../routes.jsx');
+
+React.render(<Routes />, document.getElementById('main'));
